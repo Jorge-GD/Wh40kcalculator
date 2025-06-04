4ea56547f47ff70ea85b55361238840c65b3f399
JorgeG <84985189+Jorge-GD@users.noreply.github.com>
2025-06-04 09:55:53 +0200

    Add ESLint and Prettier configuration

---
+++ .eslintrc.cjs
@@ -0,0 +1,18 @@
+module.exports = {
+  parser: '@typescript-eslint/parser',
+  plugins: ['@typescript-eslint', 'prettier'],
+  extends: [
+    'eslint:recommended',
+    'plugin:@typescript-eslint/recommended',
+    'plugin:prettier/recommended'
+  ],
+  env: {
+    browser: true,
+    es2021: true
+  },
+  parserOptions: {
+    project: './tsconfig.json',
+    sourceType: 'module'
+  },
+  ignorePatterns: ['dist/', 'node_modules/']
+};
