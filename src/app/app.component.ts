420f21691a7b4104c94dd0c8eb9515bb525f71a2
JorgeG <84985189+Jorge-GD@users.noreply.github.com>
2025-06-04 12:52:17 +0200

    feat(T-06): simplify header navigation

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,5 +1,6 @@
 import { Component, OnInit } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import { HeaderComponent } from './components/header/header.component';
 import { ThemeService } from './services/theme.service';
 import { MatSelectModule } from '@angular/material/select';
 import { MatFormFieldModule } from '@angular/material/form-field';
@@ -8,20 +9,27 @@ import { CommonModule } from '@angular/common';
 @Component({
   selector: 'app-root',
   standalone: true, // Added standalone: true
-  imports: [RouterOutlet, MatSelectModule, MatFormFieldModule, CommonModule],
+  imports: [
+    RouterOutlet,
+    MatSelectModule,
+    MatFormFieldModule,
+    CommonModule,
+    HeaderComponent,
+  ],
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.scss']
+  styleUrls: ['./app.component.scss'],
 })
 export class AppComponent implements OnInit {
   title = 'mathhammer-ng';
-  availableThemes: { name: string, value: string }[] = [];
+  availableThemes: { name: string; value: string }[] = [];
   currentTheme: string = '';
 
   constructor(private themeService: ThemeService) {}
 
   ngOnInit(): void {
     this.availableThemes = this.themeService.getAvailableThemes();
-    this.themeService.currentTheme$.subscribe((theme: string) => { // Explicitly type theme
+    this.themeService.currentTheme$.subscribe((theme: string) => {
+      // Explicitly type theme
       this.currentTheme = theme;
     });
   }
